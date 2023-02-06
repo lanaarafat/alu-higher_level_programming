4773d3f9be69556e4dde553035a45c70396e8b03
Lana <a.mutoni1@alustudent.com>
2023-02-06 05:01:12 -0800

    m

---
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+#3-print_alphabt.py
+
+"""print the alphabet in lowercase, not followed by a new line."""
+for letter in range(97, 123)
+    print("{}".format(chr(letter)), end="")
