0de3ed77e1d445e4e3c05e5f352e8ba2fe45161e
lanaarafat <a.mutoni1@alustudent.com>
2023-02-07 17:42:01 +0200

    m

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@ for z in range(0, 10):
     for a in range(z + 1, 10):
         if z == 8 and a == 9:
             print("{}{}".format(z, a))
-    break:
+    break
         print("{}{}".format(z, a), end=", ")
