73250f469f8dc52d4118f4b2aa1cd9c75196a4d3
lanaarafat <a.mutoni1@alustudent.com>
2023-02-23 16:00:00 +0200

    m

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -5,6 +5,8 @@ def add(a,b)
         a = 1
         b = 2
         sum = a +b
-    print(<a value> + <b value> = <add(a, b) value> .format(a, b, sum))
+    result = add(a, b)    
+    print(<a value> + <b value> = <result value> .format(a, b, sum))
     if __name__ == "__main__":
         # code to be executed when the module is run as the main program
+
