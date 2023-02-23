e0f3c20d8d29fd98229fad0dd7313b9ae7ba3fa2
lanaarafat <a.mutoni1@alustudent.com>
2023-02-23 10:28:50 +0200

    m

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -4,6 +4,8 @@ def add(a,b)
         a = 1
         b = 2
     print(<a value> + <b value> = <add(a, b) value>)
+    if __name__ == "__main__":
+        # code to be executed when the module is run as the main program
 >>> import 0-add    
 
 
