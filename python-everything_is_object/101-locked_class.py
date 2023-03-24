aaf5a55d49a8a92e1d23c170c7cfcc262d9a1cde
lanaarafat <a.mutoni1@alustudent.com>
2023-03-24 14:47:24 +0200

    LockedClass

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-#class 'LockedClass' with no class or object attribute, that prevents, the user from dynamically creating new instance attributes, except if the new instance attribute is called first_name
+"""
+    class 'LockedClass' with no class or object attriutes, that prevents,
+    the user from dynamically creating new instance attributes,
+    except if the new instance attribute is called first_name
+"""
 
 class LockedClass:
-    #empty LockedClass
+    """
+       empty LockedClass
+    """   
     __slots__ = ["first_name"]
