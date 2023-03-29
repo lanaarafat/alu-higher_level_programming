cea8feb76e88d77d4d9e770c5dbee46147817938
lanaarafat <a.mutoni1@alustudent.com>
2023-03-29 13:08:35 +0200

    create a table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- This script creates a table in the current database
 CREATE TABLE IF NOT EXISTS first_table(
-       	(id) INT
-       	(name) VARCHAR(256)
+       	id INT,
+        name VARCHAR(256)
 );
