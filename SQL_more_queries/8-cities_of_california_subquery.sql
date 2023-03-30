580cb1f759e5ef3cb34dea25c4b3aab15b10aa6d
lanaarafat <a.mutoni1@alustudent.com>
2023-03-30 13:46:35 +0200

    list

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- lists all cities of Cali in database
-SELECT id name FROM cities WHERE state_id IN (SELECT id FROM states WHERE name = "California") ORDER BY id ASC;
+SELECT id name FROM cities WHERE state_id IN (SELECT id FROM states WHERE name = "California") ORDER BY id;
