28e05247cabd02f3a1cca3d69a2b5293c1ff57f1
lanaarafat <a.mutoni1@alustudent.com>
2023-03-30 09:06:48 +0200

    Grant Select/Usage Privilege

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,5 @@
 -- create database and user
 CREATE IF NOT EXISTS DATABASE 'hbtn_0d_2';
 CREATE IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT on * . * TO 'user_0d_2'@'localhost';
+GRANT USAGE on * . * TO 'user_0d_2'@'localhost';
+GRANT SELECT on 'hbtn_0d_2' TO 'user_0d_2'@'localhost';
