3077ea7a8bc7cbc45bb582ac29f8dcfabab698e4
lanaarafat <a.mutoni1@alustudent.com>
2023-03-31 18:11:09 +0200

    inner join

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,6 @@
 -- lists all cities in the database
-SELECT cities.id, cities.name, states.name FROM cities AS c INNER JOIN states AS s ON c.state_id = states.id ORDER BY c.id;
+SELECT c.`id`, c.`name`, s.`name`
+  FROM `cities` AS c
+       INNER JOIN `states` AS s
+       ON c.`state_id` = s.`id`
+ ORDER BY c.`id`;
