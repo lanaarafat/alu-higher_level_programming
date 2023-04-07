f1d40f6601e6535e34342165652bb97b88b5772d
lanaarafat <a.mutoni1@alustudent.com>
2023-04-07 20:06:43 +0200

    post an email

---
+++ python-network_1/6-post_email.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+''' posts an email '''
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    print(r.text)
