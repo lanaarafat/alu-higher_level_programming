7ef5b1c2a73bb96ea1a12e90ae131828ee7b815f
lanaarafat <a.mutoni1@alustudent.com>
2023-04-07 19:36:27 +0200

    post an email

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -6,7 +6,7 @@ import sys
 if __name__ == "__main__":
     values = {'email': sys.argv[2]}
     data = parse.urlencode(values)
-    data = data.encode('ascil')
+    data = data.encode('ascii')
     req = request.Request(sys.argv[1], data)
     with request.urlopen(req) as response:
         body = response.read()
