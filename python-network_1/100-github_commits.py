6f9000103b51e4bbb27a8241645c9962bf524e9f
lanaarafat <a.mutoni1@alustudent.com>
2023-04-07 20:20:46 +0200

    2 challenges

---
+++ python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+''' 2 arguments to slove challenge '''
+
+
+from requests import get, auth
+import sys
+
+if __name__ == "__main__":
+    try:
+        repo = sys.argv[1]
+        owner = sys.argv[2]
+        url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+        response = get(url)
+        json_0 = response.json()
+        for i in range(0, 10):
+            print("{}: {}".format(json_0[i].get("sha"), json_0[i]
+                                  .get("commit").get("author").get("name")))
+    except:
+        pass
