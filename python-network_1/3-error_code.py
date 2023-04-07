15b7c9bcf23effb5394145ebae36a00172c4e738
lanaarafat <a.mutoni1@alustudent.com>
2023-04-07 19:44:35 +0200

    error code

---
+++ python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+''' displays body of response decoded in utff-8 '''
+from urllib import request, error
+import sys
+
+
+if __name__ == "__main__":
+    try:
+        with request.urlopen(sys.argv[1]) as response:
+            body = response.read()
+            print(body.decode('utf-8'))
+        except error.HTTPError as err:
+            print('Error code: {}'.format(err.code))
