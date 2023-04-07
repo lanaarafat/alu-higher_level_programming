b055efee2e58eb669f6ab8c2de92268906939468
lanaarafat <a.mutoni1@alustudent.com>
2023-04-07 20:10:10 +0200

    error code

---
+++ python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+''' didplays body of response if no errors '''
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
