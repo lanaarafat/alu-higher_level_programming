ced5d99fd703212722e5c23c8a30a2cd6c24c2d4
lanaarafat <a.mutoni1@alustudent.com>
2023-04-26 23:31:49 +0200

    test

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 ''' prints a text with 2 /n after each character '''
 
+
 def text_indentation(text):
     ''' print 2 lines after each character '''
     if type(text) is not str:
