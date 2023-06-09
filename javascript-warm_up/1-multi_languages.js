001ee7595acd7a2d3cd1b2da2f7fbe9ff46b17b3
lanaarafat <a.mutoni1@alustudent.com>
2023-06-09 16:29:59 +0200

    asdfghj

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-console.log("c");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
