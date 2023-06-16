a123877d4708901a14f4eb1f4efa754e46ecc9fe
lanaarafat <a.mutoni1@alustudent.com>
2023-06-16 22:20:41 +0200

    scrape

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const request = require('request');
+request.get(process.argv[2], (err, response) => {
+  if (err) console.log(err);
+  console.log(`code: ${response.statusCode}`);
+});
